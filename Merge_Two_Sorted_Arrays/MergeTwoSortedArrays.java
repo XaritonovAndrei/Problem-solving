8511998473c178334a61831a7adf93ea877274d2
XaritonovAndrei <Xaritonov.A.A@yandex.ru>
2025-03-13 19:15:32 +0300

    Add files via upload

--- Merge_Two_Sorted_Arrays/MergeTwoSortedArrays.java
+++ Merge_Two_Sorted_Arrays/MergeTwoSortedArrays.java
@@ -2,13 +2,16 @@ package com.problem_solving.Merge_Two_Sorted_Arrays;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class MergeTwoSortedArrays {
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
         int m = 3, n = 3;
+        System.out.println(Arrays.toString(nums1) + " merges with " + Arrays.toString(nums2));
         merge(nums1, m, nums2, n);
+        System.out.println("result is " + Arrays.toString(nums1));
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
