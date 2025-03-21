6598e74b1819dc686c019e615833e46c1826d38b
XaritonovAndrei <Xaritonov.A.A@yandex.ru>
2025-03-22 00:19:10 +0300

    Updated InvertTree.java

--- Tree/Invert_Tree/InvertTree.java
+++ Tree/Invert_Tree/InvertTree.java
@@ -1,3 +1,4 @@
+package com.problem_solving.Tree.Invert_Tree;
 
 //Given the root of a binary tree, invert the tree, and return its root.
 //Input: root = [4,2,7,1,3,6,9]
@@ -11,10 +12,57 @@
 //Input: root = []
 //Output: []
 
-class Solution {
-    public Node invertTree(Node root) {
+import com.problem_solving.Tree.BinarySearchTree;
+import com.problem_solving.Tree.Node;
 
-        if (root == null){
+public class InvertTree {
+    public static void main(String[] args) {
+
+        Node nodeA = new Node(1);
+        Node nodeB = new Node(2);
+        Node nodeC = new Node(3);
+        Node nodeD = new Node(4);
+        Node nodeE = new Node(5);
+        Node nodeF = new Node(6);
+
+        nodeA.left = nodeB;
+        nodeA.right = nodeC;
+        nodeB.left = nodeD;
+        nodeB.right = nodeE;
+        nodeC.left = nodeF;
+
+//                         A
+//                  B             C
+//              D       E         F
+
+        System.out.println("Original:");
+        System.out.println("Node A: " + nodeA.data
+                                + ", Left child(B): " + nodeA.left.data
+                                + ", Right child(C): " + nodeA.right.data);
+        System.out.println("Node B: " + nodeB.data
+                + ", Left child(D): " + nodeD.data
+                + ", Right child(E): " + nodeE.data);
+        System.out.println("Node C: " + nodeC.data
+                + ", Left child(F): " + nodeF.data);
+
+
+        invertTree(nodeA);
+
+        System.out.println("Inverted:");
+        System.out.println("Node A: " + nodeA.data
+                + ", Left child(B): " + nodeA.left.data
+                + ", Right child(C): " + nodeA.right.data);
+        System.out.println("Node B: " + nodeB.data
+                + ", Left child(D): " + nodeD.data
+                + ", Right child(E): " + nodeE.data);
+        System.out.println("Node C: " + nodeC.data
+                + ", Left child(F): " + nodeF.data);
+
+    }
+
+    public static Node invertTree(Node root) {
+
+        if (root == null) {
             return null;
         }
 
@@ -24,6 +72,5 @@ class Solution {
         root.left = right;
         root.right = left;
         return root;
-
     }
 }
