3bc8237d5974f09768898ba02dc85066a62fcdd6
Андрей <Xaritonov.A.A@yandex.ru>
2025-04-15 15:31:32 +0300

    Update MaximumProductSubarray.java

--- Maximum_Product_Subarray/MaximumProductSubarray.java
+++ Maximum_Product_Subarray/MaximumProductSubarray.java
@@ -40,4 +40,27 @@ public class MaximumProductSubarray {
         }
         return result;
     }
+
+    
+        public static int maxProduct(int[] nums) {
+        
+        int max = nums[0], min = nums[0], ans = nums[0];
+        int n = nums.length;
+        
+        for (int i = 1; i < n; i++) {
+        
+			// Swapping min and max
+            if (nums[i] < 0){
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+                
+            max = Math.max(nums[i], max * nums[i]); (-2, -12) -> -2;
+            min = Math.min(nums[i], min * nums[i]);  (-2, -12) -> -12 
+
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    } 
 }
