ffefab96a9f27c871d0f1e0009a0d76cb497bf62
Андрей <Xaritonov.A.A@yandex.ru>
2025-05-27 03:45:46 +0300

    Add files via upload

---
+++ Graph/Max_Area_of_Island/MaxAreaOfIsland.java
@@ -0,0 +1,53 @@
+package com.problem_solving.Graph.Max_Area_of_Island;
+
+//You are given an m x n binary matrix grid.
+// An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+// You may assume all four edges of the grid are surrounded by water.
+
+//The area of an island is the number of cells with a value 1 in the island.
+
+//Return the maximum area of an island in grid. If there is no island, return 0.
+
+public class MaxAreaOfIsland {
+
+    public static void main (String[] args) {
+        int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                        {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                        {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        System.out.println();
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int area = 1;
+
+        area += dfs(grid, i + 1, j);
+        area += dfs(grid, i - 1, j);
+        area += dfs(grid, i, j + 1);
+        area += dfs(grid, i, j - 1);
+        return area;
+    }
+}
+
