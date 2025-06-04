ed23f18886db5efa6ccb2a026f1c0e0a5dc85569
Андрей <Xaritonov.A.A@yandex.ru>
2025-06-04 10:13:43 +0300

    Add files via upload

---
+++ Majority_Element/MajorityElement.java
@@ -0,0 +1,36 @@
+package com.problem_solving.Majority_Element;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+    private static Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            }
+            else {
+                counts.put(num, counts.get(num)+1);
+            }
+        }
+        return counts;
+    }
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+}
